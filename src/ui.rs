6f95eca8467c61d12f43cd51d85f1a689e83f322
Cheetah <DaverDiscord@gmail.com>
2024-06-14 07:36:25 -0700

    Add tracking info pg (currently just lat and long)

--- src/ui.rs
+++ src/ui.rs
@@ -29,11 +29,42 @@ pub fn ui(data: &IssData, frame: &mut Frame) {
     lines.push(Line::from(vec![
         Span::styled("Currently tracking: ", Style::default().fg(Color::Green).add_modifier(Modifier::ITALIC)),
     ]));
+
     lines.push(Line::from(vec![
         Span::styled("International Space Station", Style::default().fg(Color::Gray).add_modifier(Modifier::BOLD)),
         Span::styled(format!("ID {}", 25544), Style::default().fg(Color::DarkGray).add_modifier(Modifier::ITALIC)),
     ]));
+    
     let currently_tracking_pg = Paragraph::new(Text::from(lines))
+        .style(Style::default().fg(Color::White))
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title("Current Tracking")
+                .border_type(BorderType::Thick)
+        );
+    frame.render_widget(currently_tracking_pg, info_layout[0]);
+
+    //Render the rightward paragraph: Contains information about the currently tracked object.
+    let mut lines = vec![];
+    lines.push(Line::from(vec![
+        Span::styled("Current information as of timestamp ", Style::default().fg(Color::Blue)),
+        Span::styled(format!("{}", data.timestamp), Style::default().fg(Color::LightBlue).add_modifier(Modifier::BOLD).add_modifier(Modifier::ITALIC))
+    ]));
+
+    //Latitude
+    lines.push(Line::from(vec![
+        Span::styled("Geo Latitude: ", Style::default().fg(Color::Gray).add_modifier(Modifier::BOLD)),
+        Span::styled(format!("{0:.2}", data.latitude), Style::default().fg(Color::Yellow)),
+    ]));
+
+    //Longitude
+    lines.push(Line::from(vec![
+        Span::styled("Geo longitude: ", Style::default().fg(Color::Gray).add_modifier(Modifier::BOLD)),
+        Span::styled(format!("{0:.2}", data.longitude), Style::default().fg(Color::Yellow)),
+    ]));
+
+    let tracking_info_pg = Paragraph::new(Text::from(lines))
         .style(Style::default().fg(Color::White))
         .block(
             Block::default()
@@ -41,7 +72,10 @@ pub fn ui(data: &IssData, frame: &mut Frame) {
                 .title("Tracking Info")
                 .border_type(BorderType::Thick)
         );
-    frame.render_widget(currently_tracking_pg, info_layout[0]);
+    
+    frame.render_widget(tracking_info_pg, info_layout[1]);
+
+
 
 
 
